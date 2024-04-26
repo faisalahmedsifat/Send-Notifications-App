658b83feef8bfc8e0e69493fb3a182db61b814c2
Faisal Ahmed Sifat <faisalahmed531@gmail.com>
2024-04-26 20:15:29 +0600

    fix: fixed ui for some components and generated profile apk

--- lib/screens/user_view.dart
+++ lib/screens/user_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:send_notification/api/firebase_auth_service.dart';
 import 'package:send_notification/api/firebase_messaging_service.dart';
 
 class UserView extends StatefulWidget {
@@ -20,6 +21,25 @@ class _UserViewState extends State<UserView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text('User View'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                  'Welcome ${widget.email} \nJust close the application and you will recieve notifications when the admins wants.'),
+            ),
+          ),
+          TextButton(
+              onPressed: () async {
+                await FirebaseAuthService().signOut();
+              },
+              child: Text('Sign Out')),
+        ],
+      ),
+    );
   }
 }
